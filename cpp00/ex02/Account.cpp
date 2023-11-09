77078a024a008fdc1651866bdb18dbc091ed9a61
JERINECK Léon <leonjerineck@gmail.com>
2023-11-09 16:06:26 +0100

    setup ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,23 +6,73 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:36:35 by ljerinec          #+#    #+#             */
-/*   Updated: 2023/11/09 13:46:05 by ljerinec         ###   ########.fr       */
+/*   Updated: 2023/11/09 14:29:25 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
-void	makeDeposit(int deposit)
+	int Account::_nbAccounts = 0;
+	int Account::_totalAmount = 0;
+	int Account::_totalNbDeposits = 0;
+	int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) : _amount(initial_deposit)
 {
-	
+	_accountIndex = Account::_nbAccounts;
 }
 
-static int	getNbAccounts(void)
+Account::~Account(void)
+{
+	return ;
+}
+
+int	Account::getNbAccounts(void)
 {
 	return (Account::_nbAccounts);
 }
 
-static int	getTotalAmount(void)
+int	Account::getTotalAmount(void)
 {
 	return (Account::_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (Account::_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (Account::_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	return ;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	Account::_totalNbDeposits++;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals++;
+	Account::_totalNbWithdrawals++;
+	return (1);
+}
+
+int	Account::checkAmount(void) const
+{
+	return (this->_amount);
+}
+
+void	Account::displayStatus(void) const
+{
+	return ;
 }
