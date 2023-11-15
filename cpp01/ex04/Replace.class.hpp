4b27a1549312496d41c9418e0a906cac43475c4c
ljerinec <leonjerineck@gmail.com>
2023-11-15 13:31:25 +0100

    ex04 OK

--- cpp01/ex04/Replace.class.hpp
+++ cpp01/ex04/Replace.class.hpp
@@ -6,26 +6,28 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 14:47:03 by ljerinec          #+#    #+#             */
-/*   Updated: 2023/11/14 16:01:48 by ljerinec         ###   ########.fr       */
+/*   Updated: 2023/11/15 13:19:50 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HUMANA_CLASS_HPP
-# define HUMANA_CLASS_HPP
+#ifndef REPLACE_CLASS_HPP
+# define REPLACE_CLASS_HPP
 
 #include "losers.hpp"
 
 class	Replace
 {
 public:
-	Replace(void);
+	Replace(char **argv);
 	~Replace(void);
-	void	doing(void);
+	void	replaceString(void);
 private:
-	int		isS1(void);
-	void	replace(void);
-	void	writeInFile(void);
-	std::string	line;
+	void	replaceOccurence(void);
+	void	fillFile(void);
+	std::string	_line;
+	std::string _filename;
+	std::string _s1;
+	std::string _s2;
 };
 
 #endif
--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 22:38:45 by ljerinec          #+#    #+#             */
-/*   Updated: 2023/11/14 15:53:20 by ljerinec         ###   ########.fr       */
+/*   Updated: 2023/11/15 13:17:01 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 int	main(int argc, char **argv)
 {
-	Replace	replace;
 	if (argc != 4)
 	{
 		std::cout << ERROR;
 		return (1);
 	}
 	else
-		replace.doing();
-	(void) argv;
+	{
+		Replace	replace(argv);
+		replace.replaceString();
+	}
 	return (0);
 }
