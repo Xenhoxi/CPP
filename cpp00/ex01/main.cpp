8b48d2c4a7a37da27af8248a73bf42d84c54e53a
JERINECK Léon <leonjerineck@gmail.com>
2023-11-24 11:33:57 +0100

    merge clean

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 18:15:36 by ljerinec          #+#    #+#             */
-/*   Updated: 2023/11/23 17:06:40 by ljerinec         ###   ########.fr       */
+/*   Updated: 2023/11/23 17:09:55 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,10 @@ int	main(void)
 		else if(buff == "EXIT")
 			break ;
 		else
+		{
+			std::cout << buff << std::endl;
 			std::cout << "Invalid command !" << std::endl;
+		}
 		buff = "";
 	}
 	return (0);
