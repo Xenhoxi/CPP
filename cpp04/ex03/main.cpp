6a9add6c761e330814368ec1de48207e1524a03b
JERINECK Léon <leonjerineck@gmail.com>
2023-12-20 14:50:01 +0100

    leaks

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 11:20:53 by ljerinec          #+#    #+#             */
-/*   Updated: 2023/12/20 14:29:04 by ljerinec         ###   ########.fr       */
+/*   Updated: 2023/12/20 14:49:21 by ljerinec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int main()
 	ICharacter* me = new Character("me");
 	
 	AMateria* tmp;
-	tmp = src->createMateria("ice");
+	tmp = 0;
 	me->equip(tmp);
-	tmp = src->createMateria("fire");
+	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
@@ -39,5 +39,6 @@ int main()
 	delete bob;
 	delete me;
 	delete src;
+	system("leaks interface");
 }
 
