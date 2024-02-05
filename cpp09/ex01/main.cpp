ee632201a0522a16ffce01b8c8d49aea2c7fdb4b
JERINECK Léon <leonjerineck@gmail.com>
2024-02-05 09:17:17 +0100

    ex00 alomost done

---
+++ cpp09/ex01/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 10:50:37 by ljerinec          #+#    #+#             */
+/*   Updated: 2024/02/04 00:06:57 by ljerinec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+	std::map<std::string, double> dataBase;
+
+	if (argc != 2)
+		return (1);
+	try
+	{
+		fill_dataBase(dataBase);
+		read_input(argv[1], dataBase);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Error : " << e.what() << std::endl;
+	}
+	return (0);
+}
